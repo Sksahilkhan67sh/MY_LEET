189c886d0044ab8c01654dcdaf0200ba345b343b
SAHIL KHAN <146698424+Sksahilkhan67sh@users.noreply.github.com>
2026-07-30 23:27:08 +0530

    Time: 446 ms (40.07%), Space: 26.2 MB (13.67%) - LeetHub

---
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.py
@@ -0,0 +1,34 @@
+class Solution:
+    def minFlips(self, s: str) -> int:
+        n = len(s)
+        s = s + s
+
+        alt1 = []
+        alt2 = []
+
+        for i in range(len(s)):
+            alt1.append('0' if i % 2 == 0 else '1')
+            alt2.append('1' if i % 2 == 0 else '0')
+
+        diff1 = diff2 = 0
+        ans = float("inf")
+
+        left = 0
+
+        for right in range(len(s)):
+            if s[right] != alt1[right]:
+                diff1 += 1
+            if s[right] != alt2[right]:
+                diff2 += 1
+
+            if right - left + 1 > n:
+                if s[left] != alt1[left]:
+                    diff1 -= 1
+                if s[left] != alt2[left]:
+                    diff2 -= 1
+                left += 1
+
+            if right - left + 1 == n:
+                ans = min(ans, diff1, diff2)
+
+        return ans
