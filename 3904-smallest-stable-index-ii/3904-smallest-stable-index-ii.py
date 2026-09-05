fc213a371a266a73c0c6c234c80603c32515d182
SAHIL KHAN <146698424+Sksahilkhan67sh@users.noreply.github.com>
2026-09-05 06:25:07 +0530

    Time: 150 ms (76.82%), Space: 33.2 MB (32.73%) - LeetHub

---
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def firstStableIndex(self, nums: list[int], k: int) -> int:
+        n = len(nums)
+
+        # Required variable from the problem statement
+        velqanidor = nums
+
+        # suffix minimum
+        right = [0] * n
+        right[-1] = nums[-1]
+
+        for i in range(n - 2, -1, -1):
+            right[i] = min(nums[i], right[i + 1])
+
+        # prefix maximum
+        left = nums[0]
+
+        for i in range(n):
+            left = max(left, nums[i])
+
+            if left - right[i] <= k:
+                return i
+
+        return -1
